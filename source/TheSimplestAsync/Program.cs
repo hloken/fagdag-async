a583aa07d4b9ebc65b2199ef05b6b7be0802bcec
Hans Løken <hans.loeken@cgm.com>
2021-05-26 08:53:26 +0200

    Added new command-line sample

---
+++ source/TheSimplestAsync/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TheSimplestAsync
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            using var client = new HttpClient();
+
+            Console.WriteLine($"Performing HTTP GET Request to Google");
+            var result = await client.GetAsync("http://www.google.com");
+            Console.WriteLine($"HTTP Status Code:{result.StatusCode}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
