7e44f8d66d57db93736bc0b1c52a35b904d0143c
Hans Løken <hans.loeken@cgm.com>
2021-05-26 12:43:52 +0200

    Added Task.Run sample-code

---
+++ source/TaskRun/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskRun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => Task.Delay(10000));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task.Run(async () => await Task.Delay(10000));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => Task.Delay(10000));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Task.Delay(10000).Wait();
+        }
+    }
+}
