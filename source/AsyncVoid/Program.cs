a73996d4731a3da94a9738249b84e0f0718c36bf
Hans Løken <hans.loeken@cgm.com>
2021-05-26 10:18:26 +0200

    Added AsyncVoid sample

---
+++ source/AsyncVoid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncVoid
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ResolveIssue(); // Cannot await a method that is async void so this is fire-and-forget
+        }
+
+        public static async void ResolveIssue()
+        {
+            await Task.Delay(10000);
+
+            Console.WriteLine("I finished!");
+        }
+    }
+}
